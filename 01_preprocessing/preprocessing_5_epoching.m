1a13e5568a508052598fd217fe84622b94d1ca09
Böttcher <BOETTCHAD@med.tu-dresden.de>
2022-07-08 19:15:12 +0200

    merging the epoching & baseline/AR script, work in progress

--- 01_preprocessing/preprocessing_5_epoching.m
+++ 01_preprocessing/preprocessing_5_epoching.m
@@ -2,9 +2,11 @@
 % Emulation pilot study 2021
 
 % This script contains: 
-% separating the data for both tasks by:
+% * separating the data for both tasks by:
 % epoching based on respective events
-% export data at different steps
+% * baseline correction
+% * remove bad epochs
+% * export data at different steps
 
 %% clear workspace
 clear;
@@ -22,61 +24,90 @@ file_path = matlab.desktop.editor.getActiveFilename;
 file_path = fileparts(file_path);
 addpath(file_path);
 
-% get data paths
+% get data paths for parent dirs
 filepath_parts = strsplit(file_path, "\");
-filepath_parts = filepath_parts(1:end-2);
-parent_dir_2 = strjoin(filepath_parts, "\");
+parent_dir = filepath_parts(1:end-1);
+parent_dir = strjoin(parent_dir, "\");
+parent_dir_2 = filepath_parts(1:end-2);
+parent_dir_2 = strjoin(parent_dir_2, "\");
+
+% add functions in parent dir
+addpath([char(parent_dir) filesep char("functions")]);
 
 % set input & output directory
 input_dir = [parent_dir_2 filesep "Emulation-Data-Output\03_parallelize_with_traj"];
-output_dir = [parent_dir_2 filesep "Emulation-Data-Output\04_epoched"];
+output_dir = [parent_dir_2 filesep "Emulation-Data-Output\"];
+output_dir_epoched = [parent_dir_2 filesep "Emulation-Data-Output\04_epoched"];
 
-subdir_const_rand = [output_dir filesep "const_rand"];
-subdir_occl = [output_dir filesep "occl_nonoccl"];
-subdir_peaks = [output_dir filesep "peaks"];
+subdir_const_rand = [output_dir_epoched filesep "const_rand"];
+subdir_occl = [output_dir_epoched filesep "occl_nonoccl"];
+subdir_peaks = [output_dir_epoched filesep "peaks"];
 
 cd(input_dir);
 
 %list all *.set files in inputpath
 % @saskia: add file ending produced by your script
-filenames = dir('*continuous*.set');
+%filenames = dir('**.set');
 
 %concatenate into one cell array
 files2read = {filenames.name};
 
-%% set parameters for peaks epoching
+%% set parameters for epoching
 
-EPOCH_LIMS = [-.8, 0.6]; 
-BASE_LIMS = [-800 -300];
-EVENT = 'S 40';
+epoch_lims_A = [-1.5 3];
+epoch_lims_B =  [-1.5 2];
+epoch_lims_peaks = [-.8, 0.6]; 
+
+event_A = {'S 23' 'S 24' 'S 27'};
+event_B = {'S 20' 'S 21'};
+event_peaks = 'S 40';
+
+base_lims_A = [-1000 0];
+base_lims_B = [-1000 0];
+base_lims_peaks = [-800 -300];
 
 %% loop through files and save epoched data
 for ind = 1:length(filenames)
     
     % todo: read data and save as tmpeeg
     
+    %% epoching & remove baseline for A, B, peaks
+
     %epoch continuous data for constant and random traj and save as
     %TMPEEG_A
-    TMPEEG_A = pop_epoch( TMPEEG, {'S 23' 'S 24' 'S 27'}, [-1.5 3], 'newname', [TMPEEG.setname '_A_epoched'], 'epochinfo', 'yes');
+    TMPEEG_A = pop_epoch( TMPEEG, event_A , epoch_lims_A, 'newname', [TMPEEG.setname '_A_epoched'], 'epochinfo', 'yes');
     TMPEEG_A.comment = TMPEEG_A.comment + "  *** epoch for traj parts";
     TMPEEG_A.setname = [filename '_epoched_A'];
-    TMPEEG_A = pop_saveset(TMPEEG_A, 'filename', TMPEEG_A.setname, 'filepath', char(savepath_epoched));
+    TMPEEG_A = pop_saveset(TMPEEG_A, 'filename', TMPEEG_A.setname, 'filepath', char(subdir_const_rand));
+    % remove baseline
+    TMPEEG_A = pop_rmbase(TMPEEG_A, base_lims_A ,[]);
+    TMPEEG_A.comment = TMPEEG_A.comment + "  *** apply baseline corr";
+    TMPEEG_A.setname = [filename '_complete_preprocessing_A'];
+    TMPEEG_A = pop_saveset(TMPEEG_A, 'filename', TMPEEG_A.setname, 'filepath', char(savepath_baseline));
 
 
     %epoch continuous data for occlusion/ non-occlusion and save as
     %TMPEEG_B
-    TMPEEG_B = pop_epoch( TMPEEG, {'S 20' 'S 21'}, [-1.5 2], 'newname', [TMPEEG.setname '_B_epoched'], 'epochinfo', 'yes');
+    TMPEEG_B = pop_epoch(TMPEEG, event_B , epoch_lims_B, 'newname', [TMPEEG.setname '_B_epoched'], 'epochinfo', 'yes');
     TMPEEG_B.comment = TMPEEG_B.comment + "  *** epoch for occl parts";
     TMPEEG_B.setname = [filename '_epoched_B'];
-    TMPEEG_B = pop_saveset(TMPEEG_B, 'filename', TMPEEG_B.setname, 'filepath', char(savepath_epoched));
+    TMPEEG_B = pop_saveset(TMPEEG_B, 'filename', TMPEEG_B.setname, 'filepath', char(subdir_occl));
+    % remove baseline
+    TMPEEG_B = pop_rmbase(TMPEEG_B, base_lims_B ,[]);
+    TMPEEG_B.comment = TMPEEG_B.comment + "  *** apply baseline corr";
+    TMPEEG_B.setname = [filename '_complete_preprocessing_B'];
+    TMPEEG_B = pop_saveset(TMPEEG_B, 'filename', TMPEEG_B.setname, 'filepath', char(savepath_baseline));
 
-    % epoch for peaks & remove baseline for these epochs (other are
-    % baseline corrected in the next script)
-    EEG_peaks = pop_epoch( EEG, {  EVENT  }, EPOCH_LIMS, 'newname', EEG.subject, 'epochinfo', 'yes');
-    EEG_peaks = eeg_checkset( EEG_peaks );
-    EEG = pop_rmbase(EEG, BASE_LIMS);
-    event_idx = find(strcmp({EEG.event.type}, EVENT));
-    % save epoched data
+    % epoch for peaks & remove baseline 
+    TMPEEG_peaks = pop_epoch(TMPEEG, { event_peaks }, epoch_lims_peaks, 'newname', TMPEEG.subject, 'epochinfo', 'yes');
+    TMPEEG_peaks = eeg_checkset( TMPEEG_peaks );
+    TMPEEG_peaks = pop_rmbase(TMPEEG_peaks, base_lims_peaks);
+    event_idx = find(strcmp({TMPEEG_peaks.event.type}, event_peaks));
+    % save epoched, baseline-corrected data
     file_name = strcat([EEG.subject, "_epoched_peaks"]);
     EEG = pop_saveset(EEG, 'filename', file_name, 'filepath', char(subdir_peaks));
+
+
+
+
 end
--- 01_preprocessing/preprocessing_6_baseline_artifactrejection.m
+++ 01_preprocessing/preprocessing_6_baseline_artifactrejection.m
@@ -35,77 +35,12 @@ cd(data_path);
 
 %% add this to loop
 
-
-    %% delete practice trials
-
-    exp_start_marker = 11;
-
-    % first, task A
-    % extract the marker_index from urevent S11
-    marker_index = 1;
-    found = false;
-    while found == false
-        if str2double(TMPEEG_A.urevent(marker_index).type(2:end)) == exp_start_marker
-            found = true;
-        else
-            marker_index = marker_index + 1;
-        end
-    end
-    % extract event index for this marker in event structure
-    event_index = 1;
-    found = false;
-    while found == false
-        if TMPEEG_A.event(event_index).bvmknum > marker_index
-            found = true;
-        else
-            event_index = event_index + 1;
-        end
-    end
-    % reduce events to those after practice, save events with practice to
-    % extra cell
-    TMPEEG_A.event_with_practice = TMPEEG_A.event;
-    TMPEEG_A.event = TMPEEG_A.event_with_practice(event_index:end);
-    %save without practice
-    TMPEEG_A.setname = [filename '_epoched_nopractice_A'];
-    TMPEEG_A = pop_saveset(TMPEEG_A, 'filename', TMPEEG_A.setname, 'filepath', char(savepath_no_practice));
-
-    % second, task B
-    % extract the marker_index from urevent S11
-    marker_index = 1;
-    found = false;
-    while found == false
-        if str2double(TMPEEG_B.urevent(marker_index).type(2:end)) == exp_start_marker
-            found = true;
-        else
-            marker_index = marker_index + 1;
-        end
-    end
-    % extract event index for this marker in event structure
-    event_index = 1;
-    found = false;
-    while found == false
-        if TMPEEG_B.event(event_index).bvmknum > marker_index
-            found = true;
-        else
-            event_index = event_index + 1;
-        end
-    end
-    % reduce events to those after practice, save events with practice to
-    % extra cell
-    TMPEEG_B.event_with_practice = TMPEEG_B.event;
-    TMPEEG_B.event = TMPEEG_B.event_with_practice(event_index:end);
-    %save without practicew
-    TMPEEG_B.setname = [filename '_epoched_nopractice_B'];
-    TMPEEG_B = pop_saveset(TMPEEG_B, 'filename', TMPEEG_B.setname, 'filepath', char(savepath_no_practice));
-
-
     %% apply baseline correction and save
 
     TMPEEG_A = pop_rmbase(TMPEEG_A, [-1000 0] ,[]);
     TMPEEG_A.comment = TMPEEG_A.comment + "  *** apply baseline corr";
     TMPEEG_A.setname = [filename '_complete_preprocessing_A'];
     TMPEEG_A = pop_saveset(TMPEEG_A, 'filename', TMPEEG_A.setname, 'filepath', char(savepath_baseline));
-
 
     TMPEEG_B = pop_rmbase(TMPEEG_B, [-1000 0] ,[]);
     TMPEEG_B.comment = TMPEEG_B.comment + "  *** apply baseline corr";
