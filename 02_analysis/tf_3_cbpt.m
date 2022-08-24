2e124002e2055c57423bef407f91063cd5f94677
Böttcher <BOETTCHAD@med.tu-dresden.de>
2022-08-24 08:52:46 +0200

    update

--- 02_analysis/tf_3_cbpt.m
+++ 02_analysis/tf_3_cbpt.m
@@ -1,6 +1,40 @@
+% Data Analysis Pursuit-Tracking and Pursuit-Occlusion Paradigm
+% Emulation pilot study 2021
 
+% This script contains: 
+% CBPT for constant vs. random trajectory, task A
+% for freqs: alpha, beta, theta
+
+% Adriana Böttcher
+% 23.08.22
 
 
 % parameters and design matrix
 % design matrix: 1s and 2s for contrast
 % dependent samples t test
+
+%% 
+clc;
+clearvars;
+
+% load fieldtrip toolbox
+addpath('R:\AG-Beste-Orga\Skripts\Toolbox\fieldtrip-20210212\');
+ft_defaults;
+
+% load custom ft toolbox
+addpath('R:\AG-Beste-Orga\Skripts\Fieldtrip\ft_mmcustom');
+
+% load local EEG configuration (electrodes, neighbours, layout)
+load fieldtrip_EEG_KJP_elec_61;
+load fieldtrip_EEG_KJP_layout_61;
+load fieldtrip_EEG_KJP_neighbours_61;
+
+% load IDs of included subjects
+load subjects;
+
+% conditions
+conds_A = {'const', 'rand1', 'rand2'};
+conds_B = {'occl', 'nonoccl'};
+
+% initialize input and output folder
+outputpath_A     = 'R:\AG-Beste-Studien\Emulation\06_analysis\Emulation-Data-Output\09_segments_A_TFT';
